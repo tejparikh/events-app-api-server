c692da91ac42b4106d20cf235cb066ec5c11cbf6
tejparikh <tej.g.parikh@gmail.com>
2021-03-18 05:05:18 +0000

    My first change

--- src/app.config.js
+++ src/app.config.js
@@ -4,7 +4,7 @@
  * DO NOT store state here - must be part of a stateless service
  */
 class AppConfig {
-    version = '1.0.1';
+    version = '2.0.1';
     team = 'Curiosity';
 }
 module.exports = AppConfig;
